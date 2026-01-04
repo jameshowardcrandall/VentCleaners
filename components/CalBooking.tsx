3c3a03d81ec67720904a007d0e27d2c143ee8999
James Crandall <james.crandall@trade.gov>
2026-01-04 11:42:50 -0500

    Book a cleaning update

--- components/CalBooking.tsx
+++ components/CalBooking.tsx
@@ -1,22 +1,23 @@
-'use client'
-import { getCalApi } from "@calcom/embed-react"
-import { useEffect } from "react"
+"use client";
+import { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
 
 export default function CalBooking() {
   useEffect(() => {
     (async function () {
-      const cal = await getCalApi({ namespace: "dryerventcleaning" })
+      const cal = await getCalApi({ namespace: "dryerventcleaning" });
       cal("floatingButton", {
         calLink: "ventcleaners/dryerventcleaning",
         config: { layout: "month_view" },
-        buttonPosition: "bottom-right"
-      })
+        buttonPosition: "bottom-right",
+        buttonText: "Book A Cleaning",
+      });
       cal("ui", {
         hideEventTypeDetails: false,
-        layout: "month_view"
-      })
-    })()
-  }, [])
+        layout: "month_view",
+      });
+    })();
+  }, []);
 
-  return null // This component doesn't render anything visible, just initializes Cal.com
+  return null; // This component doesn't render anything visible, just initializes Cal.com
 }
